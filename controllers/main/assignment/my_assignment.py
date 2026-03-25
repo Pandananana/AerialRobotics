a5340b5dca1b16ebbd7ff42558fdc77c9d06ddea
Pandananana <andreas@wend.dk>
2026-03-25 11:21:58 +0100

    fix random environment and take picture along with drone coordinates, for later computer vision development

--- controllers/main/assignment/my_assignment.py
+++ controllers/main/assignment/my_assignment.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 import cv2
+import csv
 
 # The available ground truth state measurements can be accessed by calling sensor_data[item]. All values of "item" are provided as defined in main.py within the function read_sensors.
 # The "item" values that you may later retrieve for the hardware project are:
@@ -27,6 +28,7 @@ import cv2
 class MyAssignment:
     def __init__(self):
         # ---- INITIALISE YOUR VARIABLES HERE ----
+        self.photo_counter = 0
         pass
 
     def compute_command(self, sensor_data, camera_data, dt):
@@ -39,10 +41,32 @@ class MyAssignment:
             control_command = [sensor_data['x_global'], sensor_data['y_global'], 1.0, sensor_data['yaw']]
             return control_command
 
+        if np.abs(sensor_data['z_global'] - 1.0) < 0.01:
+            if self.photo_counter == 0:
+                self.take_photo(sensor_data, camera_data)
+                self.photo_counter += 1
+
         # ---- YOUR CODE HERE ----
         control_command = [sensor_data['x_global'], sensor_data['y_global'], 1.0, sensor_data['yaw']]
 
         return control_command # Ordered as array with: [pos_x_cmd, pos_y_cmd, pos_z_cmd, yaw_cmd] in meters and radians
+
+    def take_photo(self, sensor_data, camera_data):
+        # Save the camera image
+        image_filename = "data/gate1.png"
+        cv2.imwrite(image_filename, camera_data)
+
+        # Prepare data to be saved
+        csv_file = "data/gate1.csv"
+        position = [sensor_data['x_global'], sensor_data['y_global'], sensor_data['z_global']]
+        orientation = [sensor_data['roll'], sensor_data['pitch'], sensor_data['yaw']]
+        row = position + orientation
+
+        # Overwrite CSV each time (header + current row)
+        with open(csv_file, mode='w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['x_global', 'y_global', 'z_global', 'roll', 'pitch', 'yaw'])
+            writer.writerow(row)
 
 
 # Module-level singleton so main.py can call assignment.get_command() unchanged
