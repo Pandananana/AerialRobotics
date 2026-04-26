83cefe4638e05eff3d48889ddf2e0b566238c0b9
Pandananana <andreas@wend.dk>
2026-04-26 13:51:37 +0800

    tune: vel_z P 10 -> 12 (cost 1.2598 -> 0.9392)

--- controllers/main/exercises/ex1_pid_control.py
+++ controllers/main/exercises/ex1_pid_control.py
@@ -18,7 +18,7 @@ class quadrotor_controller():
         gains = {
                     "P_pos_z": 5.0,     "I_pos_z": 0.0,     "D_pos_z": 0.8,
                     "P_pos_xy": 1.5,    "I_pos_xy": 0.0,    "D_pos_xy": 0.0,
-                    "P_vel_z": 10.0,     "I_vel_z": 0.0,     "D_vel_z": 2.0,
+                    "P_vel_z": 12.0,     "I_vel_z": 0.0,     "D_vel_z": 2.0,
                     "P_vel_xy": 1.0,    "I_vel_xy": 0.04,    "D_vel_xy": 0.045,
                     "P_att_rp": 12.0,   "I_att_rp": 0.0,    "D_att_rp": 0.3,
                     "P_att_y": 9.0,     "I_att_y": 0.0,     "D_att_y": 0.3,
