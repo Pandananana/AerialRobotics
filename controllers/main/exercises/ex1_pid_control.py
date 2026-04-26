4100ae9c75c851868807bfb90c369c74207eed2e
Pandananana <andreas@wend.dk>
2026-04-26 13:40:15 +0800

    tune: rate_y D 0.001 -> 0.0015 (cost 0.5511 -> 0.4915)
    Overshoot dropped from (6.6%,14.4%) to (6.7%,6.4%). Rise time
    slower but overall step-response cost improved.

--- controllers/main/exercises/ex1_pid_control.py
+++ controllers/main/exercises/ex1_pid_control.py
@@ -12,7 +12,7 @@ class quadrotor_controller():
         if exp_num == 4:
             self.tuning_level = "off" # Always off when in racing mode
         else:
-            self.tuning_level = "vel_z" # Modify this when tuning, to tune different things. Choose between ["vel_z", "pos_z", "vel_xy", "pos_xy"]
+            self.tuning_level = "rate_y" # Modify this when tuning, to tune different things. Choose between ["vel_z", "pos_z", "vel_xy", "pos_xy"]
         
     
         gains = {
@@ -23,7 +23,7 @@ class quadrotor_controller():
                     "P_att_rp": 10.0,   "I_att_rp": 0.0,    "D_att_rp": 0.2,
                     "P_att_y": 4.0,     "I_att_y": 0.0,     "D_att_y": 0.3,
                     "P_rate_rp": 1.5,   "I_rate_rp":0.0,    "D_rate_rp": 0.1,
-                    "P_rate_y": 0.02,   "I_rate_y": 0.0,    "D_rate_y": 0.001
+                    "P_rate_y": 0.02,   "I_rate_y": 0.0,    "D_rate_y": 0.0015
                     }
 
                     
